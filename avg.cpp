6b5d636b8f720b42fc720b0832c1a557e4c64f89
Purbasha Chowdhury <pekhom966@gmail.com>
2019-06-06 12:51:50 +0530

    third commit

---
+++ avg.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float sum,average,number1,number2;
+	cout<<"enter two numbers:";
+	cin>>number1;
+	cin>>number2;
+	
+	
+	sum=number1 + number2;
+	average=sum/2;
+	
+	
+	cout<< "Sum= " << sum;
+	cout<< "Average= " << average;
+	 return 0;		
+}
